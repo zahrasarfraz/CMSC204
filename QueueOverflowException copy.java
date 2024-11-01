3b205f8b3f0a1f48b57ccfbb41aac2c79098ad14
zahrasarfraz <149403690+zahrasarfraz@users.noreply.github.com>
2024-10-31 21:09:27 -0400

    Update QueueOverflowException copy.java

--- QueueOverflowException copy.java
+++ QueueOverflowException copy.java
@@ -1,10 +1,1 @@
-/**
- * Exception thrown if enqueue called on a full queuue
- * @author ZahraSarfraz
- */
-public class QueueOverflowException extends Exception {
 
-	public QueueOverflowException() {
-		super("The Queue is not full and under the flow");
-	}
-}
